0061eda8127898a1c4cb4fceb3fedf931b770286
Ryan Tran <trankhang2856@gmail.com>
2026-07-15 05:44:33 +0700

    phase2: gen P2.14-P2.16 (RollingRank, Argmax, Argmin)

---
+++ output/single_feat_alpha/SF_ROLLING_ARGMAX_15min.py
@@ -0,0 +1,19 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 5
+
+    def __algorithm__(self):
+        high = self.data.pv_high
+        rolling_argmax = self.feat.rolling_argmax(high, timeperiod=20)
+
+        long_setup = rolling_argmax >= 10
+        short_setup = rolling_argmax == 0
+        exit_setup = long_setup & short_setup
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
---
+++ output/single_feat_alpha/SF_ROLLING_RANK_15min.py
@@ -0,0 +1,19 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        close = self.data.pv_close
+        rolling_rank = self.feat.rolling_rank(close, timeperiod=20)
+
+        long_setup = rolling_rank < 0.1
+        short_setup = rolling_rank > 0.9
+        exit_setup = abs(rolling_rank - 0.5) < 0.2
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
