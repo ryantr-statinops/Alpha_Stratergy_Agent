3741f93282d9a2a527e42c666b73039be272db4c
Ryan Tran <trankhang2856@gmail.com>
2026-07-15 05:35:50 +0700

    phase2: gen P2.5-P2.8 (OBV, MFI, CMF, AD)

---
+++ output/single_feat_alpha/SF_MFI_15min.py
@@ -0,0 +1,22 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        high = self.data.pv_high
+        low = self.data.pv_low
+        close = self.data.pv_close
+        volume = self.data.pv_volume
+        mfi = self.feat.mfi(high, low, close, volume, timeperiod=14)
+
+        long_setup = mfi < 20
+        short_setup = mfi > 80
+        exit_setup = self.op.crossed(mfi, 50)
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
---
+++ output/single_feat_alpha/SF_OBV_15min.py
@@ -0,0 +1,21 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        close = self.data.pv_close
+        volume = self.data.pv_volume
+        obv = self.feat.obv(close, volume)
+        obv_ma = self.feat.rolling_mean(obv, timeperiod=20)
+
+        long_setup = obv > obv_ma
+        short_setup = obv < obv_ma
+        exit_setup = self.op.crossed(obv, obv_ma)
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
