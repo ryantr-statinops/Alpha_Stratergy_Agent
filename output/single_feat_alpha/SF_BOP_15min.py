bda93c325113f8f2579823e5a99a3cb4b28ac8ce
Ryan Tran <trankhang2856@gmail.com>
2026-07-15 05:32:00 +0700

    phase2: gen P2.1-P2.4 (SAR, MAMA, HT_Trendline, BOP)

---
+++ output/single_feat_alpha/SF_BOP_15min.py
@@ -0,0 +1,22 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        open_price = self.data.pv_open
+        high = self.data.pv_high
+        low = self.data.pv_low
+        close = self.data.pv_close
+        bop = self.feat.bop(open_price, high, low, close)
+
+        long_setup = bop > 0.5
+        short_setup = bop < -0.5
+        exit_setup = self.op.crossed(bop, 0)
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
---
+++ output/single_feat_alpha/SF_SAR_15min.py
@@ -0,0 +1,21 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        close = self.data.pv_close
+        high = self.data.pv_high
+        low = self.data.pv_low
+        sar = self.feat.sar(high, low, acceleration=0.02, maximum=0.2)
+
+        long_setup = sar < close
+        short_setup = sar > close
+        exit_setup = self.op.crossed(close, sar)
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
