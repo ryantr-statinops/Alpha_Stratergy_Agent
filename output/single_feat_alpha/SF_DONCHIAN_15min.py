de5cca1fc00c7ef3526ec2134422296199ca6d09
Ryan Tran <trankhang2856@gmail.com>
2026-07-15 05:37:29 +0700

    phase2: gen P2.9-P2.12 (BBands, PriceZ, Zscore, Donchian)

---
+++ output/single_feat_alpha/SF_DONCHIAN_15min.py
@@ -0,0 +1,22 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        close = self.data.pv_close
+        high = self.data.pv_high
+        low = self.data.pv_low
+        donchian_upper = self.feat.donchian_upper(high, timeperiod=20)
+        donchian_lower = self.feat.donchian_lower(low, timeperiod=20)
+
+        long_setup = close > donchian_upper
+        short_setup = close < donchian_lower
+        exit_setup = self.op.crossed(close, donchian_upper) | self.op.crossed(close, donchian_lower)
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
