49f63b9388b6ddc17ff598f8360ba37c7b56603d
Ryan Tran <trankhang2856@gmail.com>
2026-07-15 05:42:26 +0700

    phase2: gen P2.13 candlestick patterns (engulfing, morning/evening star, hammer, shooting star, marubozu, 3WS, 3BC)

---
+++ output/single_feat_alpha/SF_HAMMER_15min.py
@@ -0,0 +1,22 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 5
+
+    def __algorithm__(self):
+        open_price = self.data.pv_open
+        high = self.data.pv_high
+        low = self.data.pv_low
+        close = self.data.pv_close
+        hammer = self.feat.hammer(open_price, high, low, close)
+
+        long_setup = hammer == 1
+        short_setup = hammer == -1
+        exit_setup = long_setup & short_setup
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
