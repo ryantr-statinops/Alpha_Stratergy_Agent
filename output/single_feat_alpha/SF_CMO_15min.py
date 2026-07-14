33459c60e6b472f553617ae1da76b39901382031
Ryan Tran <trankhang2856@gmail.com>
2026-07-15 04:37:31 +0700

    create 4 new single-feat alpha files (trend-following pattern with session gating)

---
+++ output/single_feat_alpha/SF_CMO_15min.py
@@ -0,0 +1,19 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        close = self.data.pv_close
+        cmo = self.feat.cmo(close, timeperiod=14)
+
+        long_setup = cmo > 0
+        short_setup = cmo < 0
+        exit_setup = self.op.crossed(cmo, 0)
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
---
+++ output/single_feat_alpha/SF_RSI_15min.py
@@ -0,0 +1,19 @@
+class CustomStrategy(SimpleAlgorithm):
+    position_open_ranges = ["02:00-04:30", "06:00-07:20"]
+    position_close_ranges = ["04:20-04:30", "07:20-07:30"]
+    position_close_after_n_candles = 12
+
+    def __algorithm__(self):
+        close = self.data.pv_close
+        rsi = self.feat.rsi(close, timeperiod=14)
+
+        long_setup = rsi > 50
+        short_setup = rsi < 50
+        exit_setup = self.op.crossed(rsi, 50)
+
+        long_signal = long_setup & (~exit_setup)
+        short_signal = short_setup & (~exit_setup)
+
+        self.set_positions(exit_setup, position=0)
+        self.set_positions(long_signal, position=1)
+        self.set_positions(short_signal, position=-1)
